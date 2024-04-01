71675910f8ab0fb681455e09f62501e96d301b98
Pier-Olivier Clement <poclement@riyoth.info>
2024-04-01 10:30:12 -0400

    Add update manager

---
+++ bin/.local/bin/update-manager.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+remote_change () {
+	REMOTE_CHANGES=$(git rev-list --count master..remotes/origin/master)
+	if [[ "$REMOTE_CHANGES" != "0" ]]
+	then
+		echo "You are behind Main by ${REMOTE_CHANGES} commits in $(pwd)"
+	fi
+}
+
+local_change () {
+	LOCAL_CHANGES=$(git status -s)
+	if [[ -n $LOCAL_CHANGES ]]
+	then
+		echo "You have uncommited change in your $(pwd). You probably shoud clean that up."
+		echo "$LOCAL_CHANGES"
+	fi
+}
+
+check_change () {
+	old_directory=$(pwd)
+	cd $1
+	remote_change
+	local_change
+	cd $old_directory
+}
+
+main () {
+	REPOSITORY_LIST=("${XDG_DATA_HOME:-$HOME}/dotfiles" "${ZDOTDIR}/.zprezto")
+	#REPOSITORY_LIST=("$HOME/dotfile")
+	for repo in ${REPOSITORY_LIST[@]}; do
+		check_change $repo
+	done
+}
+
+main
