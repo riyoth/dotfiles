8834a0ee8a57ed1cf1feb2ef1336a291ba8b0077
Pier-Olivier Clement <poclement@riyoth.info>
2026-02-17 10:31:42 -0500

    update utils setup script

--- utils/.local/share/utils/setup.sh
+++ utils/.local/share/utils/setup.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-ln -s $XDG_DATA_HOME/utils/python-tools/mkvenv ~/.local/bin
+
+if [ ! -f "$HOME/.local/bin/mkvenv" ]; then
+	ln -s $XDG_DATA_HOME/utils/python-tools/mkvenv $HOME/.local/bin
+fi
+
+if [ ! -f "$HOME/.local/bin/stmux" ]; then
+	ln -s $XDG_DATA_HOME/utils/tmux/stmux $HOME/.local/bin
+fi
