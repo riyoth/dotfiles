db9ae631aae8d7dafa0c063846eb60d2ffedfecb
Pier-Olivier Clement <poclement@riyoth.info>
2026-05-06 20:18:56 -0400

    feat: Add AWS codeartifact script for uv

--- utils/.local/share/utils/setup.sh
+++ utils/.local/share/utils/setup.sh
@@ -12,3 +12,7 @@ fi
 if [ ! -f "$HOME/.local/bin/git-sprout" ]; then
 	ln -s $XDG_DATA_HOME/utils/git/git-sprout $HOME/.local/bin
 fi
+
+if [ ! -f "$HOME/.local/bin/uv-codeartifact" ]; then
+	ln -s $XDG_DATA_HOME/utils/python-tools/uv-codeartifact $HOME/.local/bin
+fi
